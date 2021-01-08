f7b899ef9d77c1dfdf6e8bfd95f24dabad4c21be
SimonMlt <simon-malet@hotmail.fr>
2021-01-08 12:19:42 +0100

    home + coms

--- src/client/src/views/Home.js
+++ src/client/src/views/Home.js
@@ -7,7 +7,14 @@ const Home = () => {
         <div className="home">
             <LeftNav/>
             <div className="main">
-                <p>Test</p>
+                <div class="row">
+                    <p class="text-center">
+                        Au sein de l'AgenceJS, vous pouvez retrouver tous les logements que nous proposons. Appartement, maison ou bien villa,
+                        vous avez l'embaras du choix. N'hésitez pas : rentrez votre code postal, et naviguez sur notre site pour découvrir
+                        les logements disponibles dans votre département. N'hésitez pas à nous contacter en cas de problèmes,
+                        nous nous tenons à votre entière disposition.
+                    </p>
+                </div>
             </div>
         </div>
     );
