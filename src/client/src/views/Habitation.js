a79439c8929ae09ea4ee3414b7dad7362b588315
Jeevesen <jorvencurpen92@gmail.com>
2021-01-08 14:38:30 +0100

    NewPosts

--- src/client/src/views/Habitation.js
+++ src/client/src/views/Habitation.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import LeftNav from "../components/LeftNav";
 import ListeHabitation from "../components/ListeHabitation";
+import NewPostForm from "../components/Post/NewPostForm";
+import {UidContext} from "../components/AppContext";
 
 const Habitation = () => {
+    const uid = useContext(UidContext);
+
     return (
         <div className="home">
             <LeftNav/>
             <div className="main">
+                <div className="home-header">
+                    {uid ? <NewPostForm/> : "Test"}
+                </div>
                 <ListeHabitation/>
             </div>
         </div>
--- src/client/src/views/Home.js
+++ src/client/src/views/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import LeftNav from "../components/LeftNav";
-import ListeHabitation from "../components/ListeHabitation";
 
 const Home = () => {
+
     return (
         <div className="home">
             <LeftNav/>
             <div className="main">
-                <p>Test</p>
             </div>
         </div>
     );
